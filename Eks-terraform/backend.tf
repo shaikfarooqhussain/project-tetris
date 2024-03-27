2fd8ce5e6eac35df60377c19123f7475209ca375
shaikfarooqhussain <95218875+shaikfarooqhussain@users.noreply.github.com>
2024-03-28 02:28:23 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetricsb12" # Replace with your actual S3 bucket name
+    bucket = "teticspb.1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
